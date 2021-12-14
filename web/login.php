09b92ff59c714752b716c7f8af22a033300c90eb
leanhtai01 <leanhtai01@gmail.com>
2021-12-14 11:01:52 +0700

    add login form to login.php

--- web/login.php
+++ web/login.php
@@ -5,6 +5,21 @@ $page = "login";
 
 <?php include_once "header.php" ?>
 
-<h1>This is login.php page!</h1>
+<div class="text-center">
+  <form class="login-form" action="" method="post">
+    <h1 class="mt-4">Login</h1>
+    <div class="form-floating mb-3 mt-4">
+      <input type="email" class="form-control" name="email" id="email" placeholder="Email" required autofocus>
+      <label for="email">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+      <label for="password">Password</label>
+    </div>
+    <div class="d-grid gap-2 mt-4">
+      <button class="btn btn-primary btn-lg" type="submit">Login</button>
+    </div>
+  </form>
+</div>
 
 <?php include_once "footer.php" ?>
