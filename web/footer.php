2fe8165bb9b0274a99791482b1edbae853ee5550
leanhtai01 <leanhtai01@gmail.com>
2021-12-14 09:34:32 +0700

    add initial header.php, footer.php, index.php

---
+++ web/footer.php
@@ -0,0 +1,6 @@
+  <footer>
+    &copy; 2021 Lê Anh Tài
+  </footer>
+</body>
+
+</html>
---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "leanhtai01-ecommerce";
+$page = "index";
+?>
+
+<?php include_once "header.php" ?>
+
+<h1>This is index.php page!</h1>
+
+<?php include_once "footer.php" ?>
