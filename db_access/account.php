56d2c265a37567f4ab47dafbe81470cfd1774ff9
leanhtai01 <leanhtai01@gmail.com>
2021-12-15 11:03:19 +0700

    done basic register feature

--- db_access/account.php
+++ db_access/account.php
@@ -22,7 +22,7 @@ function is_valid_login($email, $password)
 
   // try to get account's info using email
   $sql = "SELECT id, fullname, password, role_id FROM accounts "
-       . "WHERE email = :email;";
+    . "WHERE email = :email;";
   $stmt = $pdo->prepare($sql);
   $stmt->execute(["email" => $email]);
   $account_info = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -32,10 +32,79 @@ function is_valid_login($email, $password)
     if (password_verify($password, $account_info["password"])) {
       // not return the password
       unset($account_info["password"]);
-      
+
       return $account_info;
     } else {
       return false;
     }
   }
+}
+
+/**
+ * Add new account
+ *
+ * @param array $account_info New account information
+ *
+ * @return 0|1|2 Return 0 on success, 1 if email already exist, 2 if
+ * phone_number already exists
+ */
+function add_account($account_info)
+{
+  global $pdo;
+
+  if (is_email_exists($account_info["email"])) {
+    return 1;
+  }
+
+  if (is_phone_number_exists($account_info["phone_number"])) {
+    return 2;
+  }
+
+  // encrypted password
+  $account_info["password"] = password_hash($account_info["password"],
+                                            PASSWORD_DEFAULT);
+
+  // add account
+  $sql = "INSERT INTO accounts "
+       . "(fullname, email, password, phone_number, address, role_id) "
+       . "VALUES "
+       . "(:fullname, :email, :password, :phone_number, :address, :role_id);";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($account_info);
+
+  return 0;
+}
+
+/**
+ * Check whether email is exists
+ *
+ * @param string $email Email address.
+ *
+ * @return true|false Return true if the email exists, false otherwise
+ */
+function is_email_exists($email)
+{
+  global $pdo;
+  $sql = "SELECT COUNT(*) FROM accounts WHERE email = :email;";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(["email" => $email]);
+
+  return $stmt->fetchColumn() > 0;
+}
+
+/**
+ * Check whether phone_number is exists
+ *
+ * @param string $phone_number Phone number.
+ *
+ * @return true|false Return true if the phone_number exists, false otherwise
+ */
+function is_phone_number_exists($phone_number)
+{
+  global $pdo;
+  $sql = "SELECT COUNT(*) FROM accounts WHERE phone_number = :phone_number;";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(["phone_number" => $phone_number]);
+
+  return $stmt->fetchColumn() > 0;
 }
--- web/account/register.php
+++ web/account/register.php
@@ -4,6 +4,32 @@ require_once(dirname(dirname(__DIR__)) . "/db_access/account.php");
 
 $title = "Register";
 $page = "register";
+$error_code = 0;
+$error_message = "";
+
+if (isset($_POST["register_btn"])) {
+  extract($_POST, EXTR_PREFIX_ALL, 'frompost');
+
+  // create account's info associate array
+  $account_info = [
+    "fullname" => $frompost_fullname,
+    "email" => $frompost_email,
+    "password" => $frompost_password,
+    "phone_number" => $frompost_phone_number,
+    "address" => $frompost_address,
+    "role_id" => 1 // can only register as user
+  ];
+
+  $error_code = add_account($account_info);
+
+  if ($error_code == 0) { // add success
+    header("Location: " . $host_url . "account/login.php");
+  } elseif ($error_code == 1) {
+    $error_message = "Email already exists!";
+  } elseif ($error_code == 2) {
+    $error_message = "Phone number already exists!";
+  }
+}
 ?>
 
 <?php include_once(dirname(dirname(__DIR__)) . "/template/header.php") ?>
@@ -11,6 +37,13 @@ $page = "register";
 <div class="text-center register-form">
   <form class="row g-3" action="" method="post">
     <h1 class="mt-4">Register</h1>
+
+    <!-- Display error message -->
+    <?php if ($error_code != 0) : ?>
+      <div class="alert alert-danger" role="alert">
+        <?php echo $error_message; ?>
+      </div>
+    <?php endif; ?>
 
     <div class="col-md-6 form-floating">
       <input type="email" class="form-control" name="email" id="email" placeholder="Email" required autofocus>
