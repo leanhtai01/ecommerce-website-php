ff1410c1807a6603833f65ab8450e85fa55d4e08
leanhtai01 <leanhtai01@gmail.com>
2021-12-16 09:49:25 +0700

    display existing personal's info to profile settings form

--- web/account/index.php
+++ web/account/index.php
@@ -13,6 +13,11 @@ if ($_SESSION["role_id"] == 0) {
 } elseif ($_SESSION["role_id"] == 2) {
   header("Location: " . $host_url . "/account/login.php");
 }
+
+// redirect user to login page if can not get personal's info
+if (!$personal_info = get_personal_info($_SESSION["account_info"]["id"])) {
+  header("Location: " . $host_url . "/account/login.php");
+}
 ?>
 
 <?php include_once(dirname(dirname(__DIR__)) . "/template/header.php") ?>
@@ -29,19 +34,19 @@ if ($_SESSION["role_id"] == 0) {
     <?php endif; ?>
 
     <div class="col-12 form-floating">
-      <input type="email" class="form-control" name="email" id="email" placeholder="Email" required autofocus>
+      <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="<?php echo $personal_info["email"]; ?>" required autofocus>
       <label for="email">Email</label>
     </div>
     <div class="col-12 form-floating">
-      <input type="text" class="form-control" name="phone_number" id="phone_number" placeholder="Phone number" required>
+      <input type="text" class="form-control" name="phone_number" id="phone_number" placeholder="Phone number" value="<?php echo $personal_info["phone_number"]; ?>" required>
       <label for="phone_number">Phone number</label>
     </div>
     <div class="col-12 form-floating">
-      <input type="text" class="form-control" name="fullname" id="fullname" placeholder="Full name" required>
+      <input type="text" class="form-control" name="fullname" id="fullname" placeholder="Full name" value="<?php echo $personal_info["fullname"]; ?>" required>
       <label for="fullname">Full name</label>
     </div>
     <div class="col-12 form-floating">
-      <input type="text" class="form-control" name="address" id="address" placeholder="Address" required>
+      <input type="text" class="form-control" name="address" id="address" placeholder="Address" value="<?php echo $personal_info["address"]; ?>" required>
       <label for="address">Address</label>
     </div>
     <div class="col-12 d-grid gap-2 mt-4">
