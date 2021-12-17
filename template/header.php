9e9bd6d02c6019fc43620f90ae21610b27cd3f8c
leanhtai01 <leanhtai01@gmail.com>
2021-12-17 10:07:57 +0700

    create initial layout to display products in index.php

--- template/header.php
+++ template/header.php
@@ -12,8 +12,14 @@
   <!-- Style register.php -->
   <link href="<?php echo $host_url; ?>/css/register.css" rel="stylesheet" />
 
+  <!-- Style index.php -->
+  <link href="<?php echo $host_url; ?>/css/index.css" rel="stylesheet" />
+
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <!-- Bootstrap icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
--- web/index.php
+++ web/index.php
@@ -7,6 +7,54 @@ $page = "index";
 
 <?php include_once(dirname(__DIR__) . "/template/header.php") ?>
 
-<h1>This is index.php page!</h1>
+<div class="product-grid container p-0 mt-5 mb-5">
+  <h1>Top sales</h1>
+  <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card product-card">
+        <i class="bi bi-heart-fill"></i>
+        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
+          <h4 class="card-text text-danger">VND 48,000</h4>
+          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card product-card">
+        <i class="bi bi-heart"></i>
+        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
+          <h4 class="card-text text-danger">VND 48,000</h4>
+          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card product-card">
+        <i class="bi bi-heart"></i>
+        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
+          <h4 class="card-text text-danger">VND 48,000</h4>
+          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card product-card">
+        <i class="bi bi-heart"></i>
+        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
+          <h4 class="card-text text-danger">VND 48,000</h4>
+          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include_once(dirname(__DIR__) . "/template/footer.php") ?>
