10b0174534831b84475f91a38f85e9e6086ccd25
leanhtai01 <leanhtai01@gmail.com>
2021-12-19 21:21:58 +0700

    add function upload one image to AWS S3

--- utils/upload.util.php
+++ utils/upload.util.php
@@ -1,4 +1,8 @@
 <?php
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
 
 /**
  * Check whether upload mutiple image is success (size, file type, ...)
@@ -26,4 +30,37 @@ function is_upload_multiple_img_success($imgs, $allow_img_types)
   }
 
   return true;
+}
+
+/**
+ * Upload one image to AWS S3
+ *
+ * @param string $img Image to upload
+ *
+ * @param string $new_name New name for uploaded file
+ *
+ * @return mixed|null URL to uploaded file
+ */
+function upload_img_to_aws_s3($img, $new_name)
+{
+  // instantiate an Amazon S3 client
+  $s3 = new S3Client([
+    "version" => "latest",
+    "region"  => getenv("AWS_REGION")
+  ]);
+
+  $bucket = getenv("S3_BUCKET");
+
+  try {
+    $upload = $s3->upload(
+      $bucket,
+      $new_name,
+      fopen($img, "rb"),
+      "public-read"
+    );
+  } catch (S3Exception $e) {
+    echo $e->getMessage() . "\n";
+  }
+
+  return $upload->get("ObjectURL");
 }
