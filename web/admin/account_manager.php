690930db3338b441f5e1331287116f7701886a2a
leanhtai01 <leanhtai01@gmail.com>
2021-12-21 13:43:39 +0700

    add pagination to account management

--- web/admin/account_manager.php
+++ web/admin/account_manager.php
@@ -11,7 +11,20 @@ if ($_SESSION["role_id"] != 0) {
 $title = "Account Manager";
 $page = "account_manager";
 
-$accounts = get_user_accounts_info();
+$records_per_page = 5;
+$number_of_users_account = get_number_of_users_account();
+$number_of_pages = ceil($number_of_users_account / $records_per_page);
+
+// get current page
+$current_page = 1;
+if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
+  $current_page = $_GET["page"];
+}
+
+// get pagination start offset
+$start_offset = ($current_page - 1) * $records_per_page;
+
+$accounts = get_user_accounts_info_limit($start_offset, $records_per_page);
 ?>
 
 <?php include_once(dirname(dirname(__DIR__)) . "/template/header.php") ?>
@@ -64,6 +77,29 @@ $accounts = get_user_accounts_info();
           <?php endforeach; ?>
         </tbody>
       </table>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10 d-flex justify-content-center mt-4">
+      <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item <?php echo $current_page <= 1 ? "disabled" : "" ?>">
+            <a class="page-link" href="<?php echo $current_page <= 1 ? "#" : $host_url . "/admin/account_manager.php?page=" . $current_page - 1 ?>" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <?php for ($i = 1; $i <= $number_of_pages; ++$i) : ?>
+            <li class="page-item <?php echo $current_page == $i ? "active" : "" ?>">
+              <a class="page-link" href="<?php echo $host_url; ?>/admin/account_manager.php?page=<?php echo $i; ?>">
+                <?php echo $i; ?>
+              </a>
+            </li>
+          <?php endfor; ?>
+          <li class="page-item <?php echo $current_page >= $number_of_pages ? "disabled" : "" ?>">
+            <a class="page-link" href="<?php echo $current_page >= $number_of_pages ? "#" : $host_url . "/admin/account_manager.php?page=" . $current_page + 1 ?>">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
     <div class="col-md-1"></div>
   </div>
