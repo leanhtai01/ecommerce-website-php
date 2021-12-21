7f2b35fecdf4e25d2810e69baeadb0a4de050a84
leanhtai01 <leanhtai01@gmail.com>
2021-12-21 09:48:07 +0700

    add function get_product_list

---
+++ db_access/product.php
@@ -0,0 +1,28 @@
+<?php
+require_once(dirname(__DIR__) . "/conf/db.conf.php");
+
+/**
+ * Get list of product include category's name
+ */
+function get_product_list()
+{
+  global $pdo;
+
+  $sql = "SELECT "
+       . "p.id,"
+       . "p.category_id,"
+       . "c.category_name,"
+       . "p.product_name,"
+       . "p.description,"
+       . "p.price,"
+       . "p.quantity_in_stock,"
+       . "p.create_at "
+       . "FROM products p "
+       . "INNER JOIN categories c "
+       . "ON p.category_id = c.id;";
+  
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
