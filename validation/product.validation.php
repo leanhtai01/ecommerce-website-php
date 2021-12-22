3bbe8d232c3b5d57c49b958752f67ce6294ea3e2
leanhtai01 <leanhtai01@gmail.com>
2021-12-22 19:31:45 +0700

    add initial product's info validation

---
+++ validation/product.validation.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Check whether product's information is valid
+ *
+ * @param array $product_info Product's information
+ *
+ * @return bool Return true if product's information is valid, false otherwise
+ */
+function is_valid_product_info($product_info)
+{
+  return true;
+}
