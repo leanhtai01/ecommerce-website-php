e8474d6bbb757f003ac76d87bc2c2d6afadacd89
leanhtai01 <leanhtai01@gmail.com>
2021-12-22 10:20:06 +0700

    add function add_product_img

--- db_access/product.php
+++ db_access/product.php
@@ -12,18 +12,18 @@ function get_product_list()
   global $pdo;
 
   $sql = "SELECT "
-       . "p.id,"
-       . "p.category_id,"
-       . "c.category_name,"
-       . "p.product_name,"
-       . "p.description,"
-       . "p.price,"
-       . "p.quantity_in_stock,"
-       . "p.create_at "
-       . "FROM products p "
-       . "INNER JOIN categories c "
-       . "ON p.category_id = c.id;";
-  
+    . "p.id,"
+    . "p.category_id,"
+    . "c.category_name,"
+    . "p.product_name,"
+    . "p.description,"
+    . "p.price,"
+    . "p.quantity_in_stock,"
+    . "p.create_at "
+    . "FROM products p "
+    . "INNER JOIN categories c "
+    . "ON p.category_id = c.id;";
+
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
 
@@ -43,19 +43,38 @@ function add_product_info($product_info)
   global $pdo;
 
   $sql = "INSERT INTO products ("
-       . "category_id,"
-       . "product_name,"
-       . "description,"
-       . "price,"
-       . "quantity_in_stock)"    
-       . "VALUES ("
-       . ":category_id,"
-       . ":product_name,"
-       . ":description,"
-       . ":price,"
-       . ":quantity_in_stock);";
+    . "category_id,"
+    . "product_name,"
+    . "description,"
+    . "price,"
+    . "quantity_in_stock)"
+    . "VALUES ("
+    . ":category_id,"
+    . ":product_name,"
+    . ":description,"
+    . ":price,"
+    . ":quantity_in_stock);";
   $stmt = $pdo->prepare($sql);
   $stmt->execute($product_info);
+
+  return $stmt->rowCount();
+}
+
+/**
+ * Add a product's image
+ *
+ * @param array $img_info Image's information (product_id, src)
+ *
+ * @return bool Return true on success, false otherwise
+ */
+function add_product_img($img_info)
+{
+  global $pdo;
+
+  $sql = "INSERT INTO product_images (product_id, src) "
+       . "VALUES (:product_id, :src);";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($img_info);
 
   return $stmt->rowCount();
 }
