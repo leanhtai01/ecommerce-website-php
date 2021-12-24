a5b1622a9014fa37103cc58fba6f956de8f002d0
leanhtai01 <leanhtai01@gmail.com>
2021-12-24 20:32:57 +0700

    display page not found error if invalid product's id

--- web/product/product_detail.php
+++ web/product/product_detail.php
@@ -1,8 +1,20 @@
 <?php
 require_once(dirname(dirname(__DIR__)) . "/conf/init.conf.php");
+require_once(dirname(dirname(__DIR__)) . "/db_access/product.php");
 
 $title = "Product Detail";
 $page = "product_detail";
+
+$product_info = null;
+
+if (
+  !isset($_GET["id"])
+  || !($product_info = get_product_info($_GET["id"]))
+) {
+  http_response_code(404);
+  include_once(dirname(dirname(__DIR__)) . "/template/not_found.php");
+  exit();
+}
 ?>
 
 <?php include_once(dirname(dirname(__DIR__)) . "/template/header.php") ?>
