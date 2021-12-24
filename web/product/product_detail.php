287118e885df5fec7cc789fc142645f474f2eadb
leanhtai01 <leanhtai01@gmail.com>
2021-12-24 10:24:42 +0700

    add initial product detail page

---
+++ web/product/product_detail.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Product Detail";
+$page = "product_detail";
+?>
+
+<?php include_once(dirname(dirname(__DIR__)) . "/template/header.php") ?>
+
+<h1>This is product detail page!</h1>
+
+<?php include_once(dirname(dirname(__DIR__)) . "/template/footer.php") ?>
