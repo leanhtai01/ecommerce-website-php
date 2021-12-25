71610d44353b69acfb4ec2dda67edf6ecc46b0ce
leanhtai01 <leanhtai01@gmail.com>
2021-12-25 22:51:30 +0700

    add function check whether product in cart

--- db_access/cart.php
+++ db_access/cart.php
@@ -17,7 +17,7 @@ function add_to_cart($account_id, $product_id, $quantity)
   global $pdo;
 
   $sql = "INSERT INTO carts (account_id, product_id, quantity) "
-       . "VALUES (:account_id, :product_id, :quantity);";
+    . "VALUES (:account_id, :product_id, :quantity);";
   $stmt = $pdo->prepare($sql);
   $stmt->execute([
     "account_id" => $account_id,
@@ -26,4 +26,28 @@ function add_to_cart($account_id, $product_id, $quantity)
   ]);
 
   return $stmt->rowCount() > 0;
+}
+
+/**
+ * Check whether product in cart
+ *
+ * @param int $account_id Account's id
+ *
+ * @param int $product_id Product's id
+ *
+ * @return bool Return true if product in cart, false otherwise 
+ */
+function is_in_cart($account_id, $product_id)
+{
+  global $pdo;
+
+  $sql = "SELECT COUNT(*) FROM carts "
+    . "WHERE account_id = :account_id AND product_id = :product_id;";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    "account_id" => $account_id,
+    "product_id" => $product_id
+  ]);
+
+  return $stmt->fetchColumn() > 0;
 }
