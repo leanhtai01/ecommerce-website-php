f0975854af1c374b78fac665020c984ba29fe48c
leanhtai01 <leanhtai01@gmail.com>
2021-12-25 10:41:30 +0700

    add function check whether product is rated by user

--- db_access/rating.php
+++ db_access/rating.php
@@ -17,7 +17,7 @@ function add_rating($account_id, $product_id, $comment)
   global $pdo;
 
   $sql = "INSERT INTO ratings (account_id, product_id, comment) VALUES "
-       . "(:account_id, :product_id, :comment);";
+    . "(:account_id, :product_id, :comment);";
   $stmt = $pdo->prepare($sql);
   $stmt->execute([
     "account_id" => $account_id,
@@ -26,4 +26,28 @@ function add_rating($account_id, $product_id, $comment)
   ]);
 
   return $stmt->rowCount() > 0;
+}
+
+/**
+ * Check whether product is rated by user
+ *
+ * @param int $account_id Account's id
+ *
+ * @param int $product_id Product's id
+ *
+ * @return bool Return true if product is rated by user, false otherwise
+ */
+function is_rated($account_id, $product_id)
+{
+  global $pdo;
+
+  $sql = "SELECT COUNT(*) FROM ratings "
+    . "WHERE account_id = :account_id AND product_id = :product_id;";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    "account_id" => $account_id,
+    "product_id" => $product_id
+  ]);
+
+  return $stmt->fetchColumn() > 0;
 }
