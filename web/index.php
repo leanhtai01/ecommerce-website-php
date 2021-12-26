3da35aa3a4783d41f50abe9c789b1609475c1ecf
leanhtai01 <leanhtai01@gmail.com>
2021-12-26 23:04:48 +0700

    display top favorite products

--- web/index.php
+++ web/index.php
@@ -6,6 +6,7 @@ $title = "leanhtai01-ecommerce";
 $page = "index";
 
 $newest_product = get_newest_products(10);
+$top_favorite_products = get_top_favorite_products(10);
 ?>
 
 <?php include_once(dirname(__DIR__) . "/template/header.php") ?>
@@ -14,6 +15,20 @@ $newest_product = get_newest_products(10);
   <h1>Newest products</h1>
   <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
     <?php foreach ($newest_product as $product) : ?>
+      <div class="col">
+        <div class="card product-card">
+          <img class="product-img" src="<?php echo get_first_product_image_source($product["id"]); ?>" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="product-name card-title"><a class="stretched-link" href="<?php echo $host_url; ?>/product/product_detail.php?id=<?php echo $product["id"]; ?>"><?php echo $product["product_name"]; ?></a></h5>
+            <h4 class="card-text text-danger">VND <?php echo number_format($product["price"], 2); ?></h4>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <h1 class="mt-4">Top favorite products</h1>
+  <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
+    <?php foreach ($top_favorite_products as $product) : ?>
       <div class="col">
         <div class="card product-card">
           <img class="product-img" src="<?php echo get_first_product_image_source($product["id"]); ?>" class="card-img-top" alt="...">
