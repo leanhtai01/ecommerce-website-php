23a6627dba57b41f05067bf965988ba26452d2c6
leanhtai01 <leanhtai01@gmail.com>
2021-12-26 22:23:25 +0700

    display top 10 newest products

--- web/index.php
+++ web/index.php
@@ -1,59 +1,29 @@
 <?php
 require_once(dirname(__DIR__) . "/conf/init.conf.php");
+require_once(dirname(__DIR__) . "/db_access/product.php");
 
 $title = "leanhtai01-ecommerce";
 $page = "index";
+
+$newest_product = get_newest_products(10);
 ?>
 
 <?php include_once(dirname(__DIR__) . "/template/header.php") ?>
 
 <div class="product-grid container p-0 mt-5 mb-5">
-  <h1>Top sales</h1>
+  <h1>Newest products</h1>
   <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
-    <div class="col">
-      <div class="card product-card">
-        <i class="bi bi-heart-fill"></i>
-        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
-          <h4 class="card-text text-danger">VND 48,000</h4>
-          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
+    <?php foreach ($newest_product as $product) : ?>
+      <div class="col">
+        <div class="card product-card">
+          <img class="product-img" src="<?php echo get_first_product_image_source($product["id"]); ?>" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="product-name card-title"><a class="stretched-link" href="<?php echo $host_url; ?>/product/product_detail.php?id=<?php echo $product["id"]; ?>"><?php echo $product["product_name"]; ?></a></h5>
+            <h4 class="card-text text-danger">VND <?php echo number_format($product["price"], 2); ?></h4>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card product-card">
-        <i class="bi bi-heart"></i>
-        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
-          <h4 class="card-text text-danger">VND 48,000</h4>
-          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card product-card">
-        <i class="bi bi-heart"></i>
-        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
-          <h4 class="card-text text-danger">VND 48,000</h4>
-          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card product-card">
-        <i class="bi bi-heart"></i>
-        <img class="product-img" src="https://images-na.ssl-images-amazon.com/images/I/41mQtYQUzmL.jpg" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="product-name card-title">HP Pavilion Gaming 10th Gen Intel Core i5 Processor</h5>
-          <h4 class="card-text text-danger">VND 48,000</h4>
-          <button type="button" class="btn-add-to-cart btn btn-outline-primary">Add to cart</button>
-        </div>
-      </div>
-    </div>
+    <?php endforeach; ?>
   </div>
 </div>
 
