5944acfc59e9ecdbee8fa80b5b2a9eac6244c7f4
leanhtai01 <leanhtai01@gmail.com>
2022-01-02 07:27:47 +0700

    account/logout.php can not accessed by guest

--- web/account/logout.php
+++ web/account/logout.php
@@ -1,5 +1,11 @@
 <?php
 require_once(dirname(dirname(__DIR__)) . "/conf/init.conf.php");
 
+if ($_SESSION["role_id"] == 2) {
+  http_response_code(404);
+  include_once(dirname(dirname(__DIR__)) . "/template/not_found.php");
+  exit();
+}
+
 session_destroy();
 header("Location: " . $host_url . "/index.php");
