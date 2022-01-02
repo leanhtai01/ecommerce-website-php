8f2425383ff321e0c3cc3553436a89bd58be9a69
leanhtai01 <leanhtai01@gmail.com>
2022-01-02 07:29:03 +0700

    account/login.php can only accessed by guest

--- web/account/login.php
+++ web/account/login.php
@@ -3,6 +3,12 @@ require_once(dirname(dirname(__DIR__)) . "/conf/init.conf.php");
 require_once(dirname(dirname(__DIR__)) . "/db_access/account.php");
 require_once(dirname(dirname(__DIR__)) . "/db_access/cart.php");
 
+if ($_SESSION["role_id"] != 2) {
+  http_response_code(404);
+  include_once(dirname(dirname(__DIR__)) . "/template/not_found.php");
+  exit();
+}
+
 $title = "Login";
 $page = "login";
 $error = "";
